0c76b544094067f7ad9c02a8ca0a42652a180428
Lukas <lukas.ancikevicius@gmail.com>
2021-08-30 08:54:14 +0300

    extract query param, Next Link as a tag

--- pages/users/[userName].jsx
+++ pages/users/[userName].jsx
@@ -2,10 +2,10 @@ import { useRouter } from 'next/router';
 
 const UsersDetails = () => {
   const routerObj = useRouter();
+  console.log(routerObj.query.userName);
   return (
     <>
-      <h1>User Details</h1>
-      <div>This is james</div>
+      <h1>User {routerObj.query.userName} Details</h1>
     </>
   );
 };
--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -1,10 +1,16 @@
+import Link from 'next/link';
+
 const UsersPage = () => {
   return (
     <>
       <h1>Or Users</h1>
       <ul>
-        <li>User Bob</li>
-        <li>User James</li>
+        <li>
+          <Link href="/users/bob">User Bob</Link>
+        </li>
+        <li>
+          <Link href="/users/james">User James</Link>
+        </li>
       </ul>
     </>
   );
